32d0a53abb330bc0387f6f4370cf48133feddba3
walid <walid.sboui@insat.ucar.tn>
2024-07-23 21:23:00 +0100

    ⚡️feat: handle concurrent  clients

--- app/server.go
+++ app/server.go
@@ -7,6 +7,7 @@ import (
 	"net"
 	"os"
 	"strings"
+	"syscall"
 )
 
 var (
@@ -32,16 +33,63 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+
+	defer l.Close()
+
+	fmt.Println("Server is listening on", *listen)
+
+	clients:=make(map[int]net.Conn)
+	fdToConn:=make(map[int]int)
+
+
+	readFds:=&syscall.FdSet{}
+
 	for {
-		req, err := l.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
+		clearFds(readFds)
+
+		fd := int(listener.(*net.TCPListener).File().Fd())
+		syscall.FD_SET(fd, readFds)
+
+		for clientFd := range clients {
+			syscall.FD_SET(clientFd, readFds)
 		}
-		go handleRequest(req)
+
+		_, err := syscall.Select(fd+1, readFds, nil, nil, nil)
+		if err != nil {
+			fmt.Println("Error with select:", err)
+			continue
+		}
+
+		if syscall.FD_ISSET(fd, readFds) {
+			conn, err := listener.Accept()
+			if err != nil {
+				fmt.Println("Error accepting connection:", err)
+				continue
+			}
+
+			clientFd := int(conn.(*net.TCPConn).File().Fd())
+			clients[clientFd] = conn
+			fdToConn[clientFd] = clientFd
+			fmt.Println("New client connected:", clientFd)
+		}
+
+		for clientFd := range clients {
+			if syscall.FD_ISSET(clientFd, readFds) {
+				handleRequest(clients[clientFd])
+			}
+		}
 
 	}
 }
+
+
+func clearFds(fds *syscall.FdSet) {
+	for i := 0; i < len(fds.Bits); i++ {
+		fds.Bits[i] = 0
+	}
+}
+
 
 func runServer(listen string) error {
 	l, err := net.Listen("tcp", listen)
@@ -50,49 +98,41 @@ func runServer(listen string) error {
 	}
 	defer l.Close()
 	fmt.Printf("Listening on %s...\n", listen)
-	for {
-		req, err := l.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
-		}
-
-		go handleRequest(req)
-
-	}
+	return nil
 }
+
 
 func handleRequest(conn net.Conn) {
 	defer conn.Close()
 
 	reader := bufio.NewReader(conn)
-
 	for {
 		requestLine, err := reader.ReadString('\n')
 		if err != nil {
-			fmt.Println("Error reading request line:", err.Error())
+			if err.Error() != "EOF" {
+				fmt.Println("Error reading request line:", err)
+			}
 			return
 		}
 
 		requestLine = strings.TrimSpace(requestLine)
 		if requestLine == "" {
-			break
+			continue
 		}
 
+		fmt.Printf("Received command: %s\n", requestLine)
+
+		var response string
 		if requestLine == "PING" {
-			response := "+PONG\r\n"
-			_, err := conn.Write([]byte(response))
-			if err != nil {
-				fmt.Println("Error writing response:", err.Error())
-				return
-			}
+			response = "+PONG\r\n"
 		} else {
-			response := fmt.Sprintf("+%s\r\n", requestLine)
-			_, err := conn.Write([]byte(response))
-			if err != nil {
-				fmt.Println("Error writing response:", err.Error())
-				return
-			}
+			response = fmt.Sprintf("+%s\r\n", requestLine)
+		}
+
+		_, err = conn.Write([]byte(response))
+		if err != nil {
+			fmt.Println("Error writing response:", err)
+			return
 		}
 	}
 }
